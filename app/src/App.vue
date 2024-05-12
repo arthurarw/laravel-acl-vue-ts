1c00c9d9a4f66d9d5d1eb74cb895f4d54a0d48f0
Arthur Warmling <arthurwarmling1@gmail.com>
2024-05-12 18:02:19 -0300

    feat: login;

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,16 +4,6 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <aside>
-    <ul>
-      <li>
-        <router-link to="/admin">Admin Home</router-link>
-      </li>
-      <li>
-        <router-link to="/">Login</router-link>
-      </li>
-    </ul>
-  </aside>
   <RouterView />
 </template>
 
--- app/src/pages/Auth/LoginPage.vue
+++ app/src/pages/Auth/LoginPage.vue
@@ -1,7 +1,44 @@
-<script setup lang="ts">
+<script lang="ts">
+import { ref } from 'vue';
+import UserGatewayHttp from '../../infra/gateway/UserGatewayHttp';
+
+export default {
+  setup() {
+    const loading = ref(false);
+    const email = ref('');
+    const password = ref('');
+
+    const auth = () => {
+      loading.value = true;
+      const userGateway = new UserGatewayHttp;
+      userGateway.login(email.value, password.value).then(response => {
+        console.log(response);
+      }).catch(error => {
+        console.log(error);
+      }).finally(() => loading.value = false);
+    }
+
+    return {
+      email,
+      password,
+      loading,
+      auth
+    }
+  },
+}
 </script>
 
 <template>
-  <div>LoginPage</div>
-  <div><a href="/forgot-password">Esqueci a senha</a></div>
+  <div>
+    <h1>Login</h1>
+    <form action="#" method="post" @submit.prevent="auth">
+      <input type="text" name="email" placeholder="E-mail" v-model="email">
+      <input type="password" name="password" placeholder="Senha" v-model="password">
+      <button type="submit" :disabled="loading">
+        <span v-if="loading">Logando...</span>
+        <span v-else>Login</span>
+      </button>
+    </form>
+    <a href="/forgot-password">Esqueci a senha</a>
+  </div>
 </template>
