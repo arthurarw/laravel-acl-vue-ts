7525a04677d5056cf3a80c3594aa7aa658dda914
Arthur Warmling <arthurwarmling1@gmail.com>
2024-05-12 18:34:24 -0300

    feat: error message;

--- app/src/pages/Auth/LoginPage.vue
+++ app/src/pages/Auth/LoginPage.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 import { useUsersStore } from '../../stores/users';
+import router from '../../router';
 
 export default {
   setup() {
@@ -8,13 +9,20 @@ export default {
     const loading = ref(false);
     const email = ref('');
     const password = ref('');
+    const error = reactive({
+      errorMessage: '',
+      status: 0,
+      active: false
+    });
 
     const auth = () => {
       loading.value = true;
-      usersStore.auth(email.value, password.value).then(response => {
-        console.log(response);
-      }).catch(error => {
-        console.log(error);
+      usersStore.auth(email.value, password.value).then(() => {
+        router.push({ name: 'admin.home' });
+      }).catch(err => {
+        error.errorMessage = err.response.data.message;
+        error.status = err.response.status;
+        error.active = true;
       }).finally(() => loading.value = false);
     }
 
@@ -22,7 +30,8 @@ export default {
       email,
       password,
       loading,
-      auth
+      auth,
+      error
     }
   },
 }
@@ -32,6 +41,9 @@ export default {
   <div>
     <h1>Login</h1>
     <form action="#" method="post" @submit.prevent="auth">
+      <div v-if="error.active">
+        <p>{{ error.errorMessage }}</p>
+      </div>
       <input type="text" name="email" placeholder="E-mail" v-model="email">
       <input type="password" name="password" placeholder="Senha" v-model="password">
       <button type="submit" :disabled="loading">
