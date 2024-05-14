7367d0041a43da7ac217b90006f0f59b20fce72a
Arthur Warmling <arthurwarmling1@gmail.com>
2024-05-13 22:49:55 -0300

    feat: middleware on routes;

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,9 +1,36 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  NavigationGuardNext,
+  RouteLocationNormalized,
+  createRouter,
+  createWebHistory,
+} from "vue-router";
 import { routes } from "./routers.map";
+import { useUsersStore } from "@/stores/users";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    _: RouteLocationNormalized,
+    next: NavigationGuardNext,
+  ) => {
+    const userStore = useUsersStore();
+    const user = userStore.me;
+    if (user?.id) {
+      return next();
+    }
+
+    const routeName = to.name;
+    if (routeName !== "auth.login" && routeName !== "auth.forgot.password") {
+      return router.push({ name: "auth.login" });
+    }
+
+    next();
+  },
+);
 
 export default router;
