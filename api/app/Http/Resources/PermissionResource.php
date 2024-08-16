5ab3e59716ffce11e9e6976973e0227a51c9265e
Arthur Warmling <arthurwarmling1@gmail.com>
2024-08-16 11:01:49 -0300

    fix: PermissionResource.php

--- api/app/Http/Resources/PermissionResource.php
+++ api/app/Http/Resources/PermissionResource.php
@@ -17,6 +17,7 @@ class PermissionResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'route_name' => $this->route_name,
             'description' => $this->description,
         ];
     }
